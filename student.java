d5d389f1b629dfede8a8791ee25ef58b5d915226
renup9845 <110659991+renup9845@users.noreply.github.com>
2022-08-25 14:08:56 +0530

    Create student.java

---
+++ student.java
@@ -0,0 +1,16 @@
+package com.bankApp;
+
+public class student {
+    int studentId;
+    int studentAge;
+    String studentName;
+    int studentMarks;
+
+    public student(int studentId, int studentAge,String studentName,int studentMarks){
+        this.studentId = studentId;
+        this.studentAge = studentAge;
+        this.studentName = studentName;
+        this.studentMarks = studentMarks;
+    }
+
+}
